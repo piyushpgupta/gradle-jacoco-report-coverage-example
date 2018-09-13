2655c25990119d10152607980d9698113c403649
piyush <piyushguptaiet@gmail.com>
2018-09-13 20:10:30 +0530

    Updated Constant
    Signed-off-by: piyushguptaiet@gmail.com

--- src/main/java/example/Greeting.java
+++ src/main/java/example/Greeting.java
@@ -1,14 +1,15 @@
 package example;
 
 public class Greeting {
+  static final String HELLO="Hello World";
   public void coveredByUnitTest() {
-   System.out.println("Hello, world.");
+   System.out.println(HELLO);
   }
 
   public void notCoveredByUnitTest() {
-   System.out.println("Hello, world.");
+   System.out.println(HELLO);
   }
   public void notCoveredByUnitTest2() {
-   System.out.println("Hello, world.");
+   System.out.println(HELLO);
   }
 }
